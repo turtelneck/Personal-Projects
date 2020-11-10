e12723b9f6fd2867521cb5b21a4a2efd636b0cc7
turtelneck <65428036+turtelneck@users.noreply.github.com>
2020-11-10 07:08:02 -0700

    create pythagorean-theorum

---
+++ pythagorean-theorum/JS/main.js
@@ -0,0 +1,27 @@
+function pythagoreanTheorum(a, b) {
+	
+	function pow(x) { // replaces Math.pow()
+		return x ** 2;
+	}
+	
+	function sqrt(x) { // replaces Math.sqrt()
+		
+		function closeEnough(guess) { // checks to see if number works as a solution
+			return Math.abs(guess * guess - x) / x < 0.001; // sqrt 
+		}
+		
+		function improve(guess) { // improves number if it doesn't work as a solution
+			return (guess + x / guess) / 2;
+		}
+		// if the current guess works, it's returned as a string rounded to the hundredth's place (fixed-point notation)
+		// if the current guess doesn't work, recur
+		function newTry(guess) {
+			return (closeEnough(guess)) ? guess.toFixed(2) : newTry(improve(guess))
+		}
+		// the first guess is just the first number divided in half
+		return newTry(x / 2);
+	}
+	// theorum is "a^2 + b^2 = c^2"
+	// the magic piece of code that gets the solution is `sqrt(pow(a) + pow(b))`
+	document.getElementById("solution").innerHTML = "the square root of the hypotenuse is... " + sqrt(pow(a) + pow(b))
+}
