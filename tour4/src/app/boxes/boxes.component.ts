015eef7dbf718d921018521f18de5f336a1043bd
turtelneck <65428036+turtelneck@users.noreply.github.com>
2022-01-18 01:28:25 -0500

    add .box-img-container
    added logic so that the image corresponding to a product card appears when you hover over the product card.

--- tour4/src/app/boxes/boxes.component.ts
+++ tour4/src/app/boxes/boxes.component.ts
@@ -11,7 +11,11 @@ import { BoxService } from '../box.service';
 })
 export class BoxesComponent implements OnInit {
   boxes: Box[] = [];
+  
+  // properties for css
   gradient: string = '';
+  displayType: string = 'none';
+  selectedBox: number = -1;
 
   constructor(private boxService: BoxService) { }
 
@@ -25,12 +29,22 @@ export class BoxesComponent implements OnInit {
     .subscribe(boxes => this.boxes = boxes);
   }
 
-  pickGradient(): void {
-    this.gradient = `var(--gradient${this.getRandomInt(6).toString()}) center/180%`;
+  pickGradient(boxId: number): void {
+    let gradientId = this.getRandomInt(6);
+
+    // show gradient in card
+    this.gradient = `var(--gradient${gradientId.toString()}) center/180%`;
+    
+    // show image of box associated with card
+    // -10 required because object ids start at 11
+    this.selectedBox = boxId-10;
   }
 
   removeGradient(): void {
+    // remove gradient
     this.gradient = 'white';
+    // hide image
+    this.selectedBox = -1;
   }
 
   getRandomInt(max: number): number {
