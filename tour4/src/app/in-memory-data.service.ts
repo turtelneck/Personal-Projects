e8adbc5fc6561d72c416f40c1dc55c10f081add4
turtelneck <65428036+turtelneck@users.noreply.github.com>
2022-01-22 21:42:42 -0500

    update images
    added some images

--- tour4/src/app/in-memory-data.service.ts
+++ tour4/src/app/in-memory-data.service.ts
@@ -8,24 +8,15 @@ import { Box } from './box';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const boxes: Box[] = [
-      { id: 11, name: 'Stō Stacker, Mk II', weight: 7, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'sto stacker mark ii' },
-      { id: 12, name: 'Stō Stacker, Mk I', weight: 8, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'sto stacker mark i' },
-      { id: 13, name: 'Big Stō', weight: 17, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'big sto box' },
-      { id: 14, name: 'Big Big Stō', weight: 23, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'big big sto box' },
-      { id: 15, name: 'Old Stō L', weight: 1, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'large cardboard box' },
-      { id: 16, name: 'Old Stō M', weight: 1, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'medium cardboard box' },
-      { id: 17, name: 'Old Stō S', weight: 1, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'small cardboard box' },
-      { id: 18, name: 'Stō Zerō', weight: 0, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'sto zero box' }
+      { id: 10, name: 'Stō Stacker, Mk II', weight: 7, imageAddress: 'assets/img/sto-stacker-mark-ii.jpg', imageAltDescription: 'sto stacker mark ii' },
+      { id: 11, name: 'Stō Stacker, Mk I', weight: 8, imageAddress: 'assets/img/sto-stacker-mark-i.jpg', imageAltDescription: 'sto stacker mark i' },
+      { id: 12, name: 'Big Stō', weight: 17, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'big sto box' },
+      { id: 13, name: 'Big Big Stō', weight: 23, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'big big sto box' },
+      { id: 14, name: 'Old Stō L', weight: 1, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'large cardboard box' },
+      { id: 15, name: 'Old Stō M', weight: 1, imageAddress: 'assets/img/old-sto-M.jpg', imageAltDescription: 'medium cardboard box' },
+      { id: 16, name: 'Old Stō S', weight: 1, imageAddress: 'assets/img/old-sto-S.jpg', imageAltDescription: 'small cardboard box' },
+      { id: 17, name: 'Stō Zerō', weight: 0, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'sto zero box' }
     ];
     return {boxes};
-  }
-
-  // Overrides the genId method to ensure that a box always has an id.
-  // If the boxes array is empty,
-  // the method below returns the initial number (11).
-  // if the boxes array is not empty, the method below returns the highest
-  // box id + 1.
-  genId(boxes: Box[]): number {
-    return boxes.length > 0 ? Math.max(...boxes.map(box => box.id)) + 1 : 11;
   }
 }
