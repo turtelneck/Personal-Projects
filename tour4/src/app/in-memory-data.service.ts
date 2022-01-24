5b64608a27a489cf3b45ffd8f9cc0ac7182e442e
turtelneck <65428036+turtelneck@users.noreply.github.com>
2022-01-23 19:38:34 -0500

    update in-memory-data.service.ts
    fixed error where old-sto-S.jpeg wasn't displaying

--- tour4/src/app/in-memory-data.service.ts
+++ tour4/src/app/in-memory-data.service.ts
@@ -14,7 +14,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 13, name: 'Big Big Stō', weight: 23, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'big big sto box' },
       { id: 14, name: 'Old Stō L', weight: 1, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'large cardboard box' },
       { id: 15, name: 'Old Stō M', weight: 1, imageAddress: 'assets/img/old-sto-M.jpg', imageAltDescription: 'medium cardboard box' },
-      { id: 16, name: 'Old Stō S', weight: 1, imageAddress: 'assets/img/old-sto-S.jpg', imageAltDescription: 'small cardboard box' },
+      { id: 16, name: 'Old Stō S', weight: 1, imageAddress: 'assets/img/old-sto-S.jpeg', imageAltDescription: 'small cardboard box' },
       { id: 17, name: 'Stō Zerō', weight: 0, imageAddress: 'assets/img/old-sto-L.jpg', imageAltDescription: 'sto zero box' }
     ];
     return {boxes};
