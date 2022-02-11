3a7cfd5a8be3ddc758482bc74c5b67dfcd904d85
turtelneck <65428036+turtelneck@users.noreply.github.com>
2022-02-10 23:15:26 -0500

    added prices
    getting rid of placeholders

--- tour4/src/app/in-memory-data.service.ts
+++ tour4/src/app/in-memory-data.service.ts
@@ -10,16 +10,16 @@ export class InMemoryDataService implements InMemoryDbService {
     const boxes: Box[] = [
       { 
         id: 10,
-        name: 'Stō Preem, Mk I',
-        price: 'priceless',
+        name: 'Stō Prem Mk I',
+        price: '160.99',
         search: 'Sto Preem, Mk I',
         imageAddress: 'assets/img/sto-preem-mk-1.jpg',
         imageAltDescription: 'sto preem mk i' 
       },
       {
         id: 11,
-        name: 'Stō Preem Lite',
-        price: 'priceless',
+        name: 'Stō Prem Lite',
+        price: '110.99',
         search: 'Sto Preem Lite',
         imageAddress: 'assets/img/sto-preem-lite.jpg',
         imageAltDescription: 'sto preem lite'
@@ -27,7 +27,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 12,
         name: 'Cargo Stō',
-        price: 'priceless',
+        price: '4150.99',
         search: 'Cargo Sto',
         imageAddress: 'assets/img/cargo-sto.jpg',
         imageAltDescription: 'cargo sto'
@@ -35,7 +35,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 13,
         name: 'Card Stō',
-        price: 'priceless',
+        price: '5.99',
         search: 'Card Sto',
         imageAddress: 'assets/img/card-sto.jpg',
         imageAltDescription: 'card sto'
@@ -43,7 +43,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 14,
         name: 'Scrapwood Stō',
-        price: 'priceless',
+        price: '12.99',
         search: 'Scrapwood Sto',
         imageAddress: 'assets/img/scrapwood-sto.jpg',
         imageAltDescription: 'scrapwood sto'
@@ -51,7 +51,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 15,
         name: 'Scrap Stō',
-        price: 'priceless',
+        price: '1310.99',
         search: 'Scrap Sto',
         imageAddress: 'assets/img/scrap-sto.jpg',
         imageAltDescription: 'scrap sto'
@@ -59,7 +59,7 @@ export class InMemoryDataService implements InMemoryDbService {
       {
         id: 16,
         name: 'Treasure Stō',
-        price: 'priceless',
+        price: '88.99',
         search: 'Treasure Sto',
         imageAddress: 'assets/img/treasure-sto.jpg',
         imageAltDescription: 'treasure sto'
