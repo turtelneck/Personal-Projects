139ded7480c9a4fd284885d07f28f152755bc554
turtelneck <65428036+turtelneck@users.noreply.github.com>
2022-02-10 23:00:23 -0500

    add price
    updated box.ts, boxes.html, in-memory-box-service.ts

--- tour4/src/app/box.ts
+++ tour4/src/app/box.ts
@@ -1,6 +1,7 @@
 export interface Box {
   id: number;
   name: string;
+  price: string;
   search: string;
   imageAddress: string;
   imageAltDescription: string;
--- tour4/src/app/in-memory-data.service.ts
+++ tour4/src/app/in-memory-data.service.ts
@@ -8,14 +8,69 @@ import { Box } from './box';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const boxes: Box[] = [
-      { id: 10, name: 'Stō Preem, Mk I', search: 'Sto Preem, Mk I', imageAddress: 'assets/img/sto-preem-mk-1.jpg', imageAltDescription: 'sto preem mk i' },
-      { id: 11, name: 'Stō Preem Lite', search: 'Sto Preem Lite', imageAddress: 'assets/img/sto-preem-lite.jpg', imageAltDescription: 'sto preem lite' },
-      { id: 12, name: 'Cargo Stō', search: 'Cargo Sto', imageAddress: 'assets/img/cargo-sto.jpg', imageAltDescription: 'cargo sto' },
-      { id: 13, name: 'Card Stō', search: 'Card Sto', imageAddress: 'assets/img/card-sto.jpg', imageAltDescription: 'card sto' },
-      { id: 14, name: 'Scrapwood Stō', search: 'Scrapwood Sto', imageAddress: 'assets/img/scrapwood-sto.jpg', imageAltDescription: 'scrapwood sto' },
-      { id: 15, name: 'Scrap Stō', search: 'Scrap Sto', imageAddress: 'assets/img/scrap-sto.jpg', imageAltDescription: 'scrap sto' },
-      { id: 16, name: 'Treasure Stō', search: 'Treasure Sto', imageAddress: 'assets/img/treasure-sto.jpg', imageAltDescription: 'treasure sto' },
-      { id: 17, name: 'Wally', search: 'Wally', imageAddress: 'assets/img/wally.jpg', imageAltDescription: 'wally' }
+      { 
+        id: 10,
+        name: 'Stō Preem, Mk I',
+        price: 'priceless',
+        search: 'Sto Preem, Mk I',
+        imageAddress: 'assets/img/sto-preem-mk-1.jpg',
+        imageAltDescription: 'sto preem mk i' 
+      },
+      {
+        id: 11,
+        name: 'Stō Preem Lite',
+        price: 'priceless',
+        search: 'Sto Preem Lite',
+        imageAddress: 'assets/img/sto-preem-lite.jpg',
+        imageAltDescription: 'sto preem lite'
+      },
+      {
+        id: 12,
+        name: 'Cargo Stō',
+        price: 'priceless',
+        search: 'Cargo Sto',
+        imageAddress: 'assets/img/cargo-sto.jpg',
+        imageAltDescription: 'cargo sto'
+    },
+      {
+        id: 13,
+        name: 'Card Stō',
+        price: 'priceless',
+        search: 'Card Sto',
+        imageAddress: 'assets/img/card-sto.jpg',
+        imageAltDescription: 'card sto'
+    },
+      {
+        id: 14,
+        name: 'Scrapwood Stō',
+        price: 'priceless',
+        search: 'Scrapwood Sto',
+        imageAddress: 'assets/img/scrapwood-sto.jpg',
+        imageAltDescription: 'scrapwood sto'
+    },
+      {
+        id: 15,
+        name: 'Scrap Stō',
+        price: 'priceless',
+        search: 'Scrap Sto',
+        imageAddress: 'assets/img/scrap-sto.jpg',
+        imageAltDescription: 'scrap sto'
+    },
+      {
+        id: 16,
+        name: 'Treasure Stō',
+        price: 'priceless',
+        search: 'Treasure Sto',
+        imageAddress: 'assets/img/treasure-sto.jpg',
+        imageAltDescription: 'treasure sto'
+    },
+      {
+        id: 17,
+        name: 'Wally',
+        search: 'Wally',
+        price: 'priceless',
+        imageAddress: 'assets/img/wally.jpg',
+        imageAltDescription: 'wally the walrus' }
     ];
     return {boxes};
   }
